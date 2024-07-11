7a2b7db42f3a0ba144b727aed0bbd4713e1d8ae7
saddam-satria <ardhisaddam@gmail.com>
2024-07-11 13:24:25 +0700

    add: message not found

--- api/src/common/MessageProperties.ts
+++ api/src/common/MessageProperties.ts
@@ -1,9 +1,17 @@
-import { SUCCESS_MESSAGE } from 'src/utils/messages';
+import {
+  NOT_FOUND_MESSAGE,
+  SUCCESS_MESSAGE,
+  UNKNOWN_ERROR_MESSAGE,
+} from 'src/utils/messages';
 
 const MessagesResponse = (key: string, lang: 'en' | 'in'): string => {
   switch (key) {
     case SUCCESS_MESSAGE.key:
       return SUCCESS_MESSAGE.message[lang];
+    case NOT_FOUND_MESSAGE.key:
+      return NOT_FOUND_MESSAGE.message[lang];
+    case UNKNOWN_ERROR_MESSAGE.key:
+      return UNKNOWN_ERROR_MESSAGE.message[lang];
     default:
       return 'success';
   }
